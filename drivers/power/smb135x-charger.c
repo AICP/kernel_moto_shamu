8a28f33cfe3342f9cf8c3d2552043c8e55c31a0c
flar2 <asegaert@gmail.com>
2015-10-22 22:56:17 +0300

    Nexus 6: USB fastcharge
    Conflicts:
    	drivers/power/smb135x-charger.c
    
    Change-Id: Ibef196c08f995c8adb26ae2d2655e599feac7a48
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -30,6 +30,10 @@
 #include <linux/reboot.h>
 #include <linux/qpnp/qpnp-adc.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
+
 #define SMB135X_BITS_PER_REG	8
 
 /* Mask/Bit helpers */
@@ -1535,6 +1539,12 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 		goto out;
 	}
 	if (current_ma == CURRENT_500_MA) {
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge)
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
+		else
+#endif
 		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
